159292f07581e17277f92d9e6ee327b7075f198f
emediong.etuks <emediong.etuks@gmail.com>
2023-11-13 10:03:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Learning1;
+
+
+var sampleData = new Sentiment.ModelInput()
+{
+     Col0 = "shotup hmm",
+};
+
+
+var result =  Sentiment.Predict(sampleData);
+
+
+var sentiment = result.PredictedLabel == 1 ? "Postive" : "Negative";
+
+
+
+Console.WriteLine(sentiment);
